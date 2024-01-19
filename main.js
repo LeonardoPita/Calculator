d0b307d76647141702be63cb175e98fb7bfc6b7c
Leonardo Pita <Leonardopita02@gmail.com>
2024-01-18 22:10:38 -0300

    Need to solve 'checkSymbol function and some other bugs'

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ const deleteBtn = document.querySelector('[data-delete]')
 const equalsBtn = document.querySelector('[data-equal]')
 const previousText = document.querySelector('[data-previous]')
 const currentText = document.querySelector('[data-current]')
+let message = false
 
 numbersBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', (e) => {
+        handleErrorMessage()
+        if (e.target.textContent === '.') {
+            let arrayCurrent = Array.from(currentText.textContent)
+            if (arrayCurrent.includes('.')) return
+        }
         currentText.textContent += btn.textContent
     })
 })
@@ -28,27 +34,70 @@ function allClear() {
 }
 
 function deleteOne() {
+    handleErrorMessage()
     if (!currentText.textContent) return
     let newUpdatedContent = currentText.textContent.slice(0, -1)
     currentText.textContent = newUpdatedContent
 }
 
 function operations(btn) {
+    handleErrorMessage()
     if (!currentText.textContent) return;
+    checkSymbols(btn)
     currentText.textContent += btn.textContent;
     previousText.textContent = currentText.textContent;
     currentText.textContent = '';
 }
 
-function endsWithOperator(myString) {
-    const lastChar = myString.charAt(myString.length - 1);
-    return ['+', '-', '*', '÷'].includes(lastChar);
-}
 
 function calculate() {
-
+    let operandA = previousText.textContent.slice(0, -1);
+    let operation = previousText.textContent.slice(-1);
+    let operandB = currentText.textContent;
+    if (operandA && operation && operandB) {
+        operandA = parseFloat(operandA);
+        operandB = parseFloat(operandB);
+        let result;
+        switch (operation) {
+            case '+':
+                result = operandA + operandB;
+                break;
+            case '-':
+                result = operandA - operandB;
+                break;
+            case '*':
+                result = operandA * operandB;
+                break;
+            case '÷':
+                if (operandB === 0) {
+                    currentText.textContent = 'Error';
+                    message = true
+                    return;
+                }
+                result = operandA / operandB;
+                break;
+            default:
+                currentText.textContent = 'Error';
+                message = true
+                return;
+        }
+        currentText.textContent = '';
+        previousText.textContent = result;
+    }
 }
 
-function equals() {
-
+function handleErrorMessage() {
+    if (message) {
+        currentText.textContent = ''
+        message = false
+    }
 }
+
+function checkSymbols(btn) {
+    if (['-', '+', '*', '÷'].includes(previousText.textContent.slice(-1))) {
+        const lastChar = previousText.textContent.slice(0, -1);
+        previousText.textContent = lastChar + btn.textContent;
+    }
+}
+
+
